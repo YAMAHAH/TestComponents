c36dfada8ada872bfb0cee3fbee853084ecb193a
YAMAHAH <75268967@QQ.COM>
2017-09-17 14:57:37 +0800

    支持嵌入PDF显示

--- src/app/common/iframe/iframe.component.ts
+++ src/app/common/iframe/iframe.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, ViewChild, ElementRef, OnInit } from '@angular/core';
+
+import { Component, Input, ViewChild, ElementRef, OnInit, OnDestroy } from '@angular/core';
 import { DomSanitizer, SafeResourceUrl } from "@angular/platform-browser";
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Http, RequestOptions, Headers } from '@angular/http';
@@ -9,26 +10,32 @@ import { Http, RequestOptions, Headers } from '@angular/http';
     templateUrl: 'iframe.component.html',
     styleUrls: ['iframe.component.css']
 })
-export class IframeComponent implements OnInit {
-    @Input() src: string;
-    safeSrc: SafeResourceUrl;
+export class IframeComponent implements OnInit, OnDestroy {
 
-    //?file=qbs.pdf
-    constructor(private sanitizer: DomSanitizer, private http: Http) {
-        this.getPdf();
-        this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl("/assets/pdfjs/web/viewer.html");
-        setTimeout(() => this.print(), 5000);
+    @Input() src: string;
+    @Input() title: string = "标题";
+    pdfViewerSrc: SafeResourceUrl;
+    pdfSrc: SafeResourceUrl;
+    constructor(private sanitizer: DomSanitizer,
+        private http: Http,
+        private httpClient: HttpClient) {
     }
     ngOnInit() {
-
+        this.getDefaultUrl();
+        setTimeout(() => this.print(), 5000);
+        setTimeout(() => this.getPdfBlobUrl(null, null), 15000);
+        setTimeout(() => this.print(), 25000);
     }
-    @ViewChild("iframeContainer", { read: ElementRef }) iframeContainer: ElementRef;
+    ngOnDestroy(): void {
+        URL.revokeObjectURL(this.fileUrl);
+    }
+    @ViewChild("pdfViewer", { read: ElementRef }) pdfViewerRef: ElementRef;
+    @ViewChild("pdfPlugin", { read: ElementRef }) pdfPluginRef: ElementRef;
     print() {
-        if (this.iframeContainer) this.iframeContainer.nativeElement.contentWindow.print();
+        if (this.pdfViewerRef)
+            this.pdfViewerRef.nativeElement.contentWindow.print();
     }
     default_url: string | Uint8Array;
-    file_url: string;
-    isLoad: boolean = true;
     async getPdf() {
         let requestHeaders = new Headers();
         requestHeaders.append('Content-Type', 'application/json');
@@ -42,19 +49,17 @@ export class IframeComponent implements OnInit {
 
         this.http.get('/api/Users/GetPdf', options)
             .subscribe(res => {
-                //支持字符串,arrayBuffer,Url
-                this.file_url = "MaterialPreparation.pdf";
                 localStorage.setItem("default_url", res.text());
             });
     }
-    dataURLtoBlob(data: any) {//data是文件流
+    dataURLtoBlob(data: string) {
         var bstr = atob(data);//解码
         var n = bstr.length;
         var u8arr = new Uint8Array(n);
         while (n--) {
             u8arr[n] = bstr.charCodeAt(n);//转二进制
         }
-        return new Blob([u8arr], { type: 'application/pdf' });//用blob生成pdf文件,返回PDF文件
+        return new Blob([u8arr], { type: 'application/pdf' });
     }
     convertToBinary(base64str: string) {
         let raw = atob(base64str);
@@ -64,28 +69,49 @@ export class IframeComponent implements OnInit {
         }
         return data;
     }
-
-    convertDataURIToBinary(dataURI: string) {   //编码转换，回答问题3
-        let BASE64_MARKER = ';base64,';
-        let base64Index = 0; //dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-        let base64 = dataURI.substring(base64Index);
-        let raw = atob(base64);
-        let rawLength = raw.length;
-        //转换成pdf.js能直接解析的Uint8Array类型,见pdf.js-4068
-        let array = new Uint8Array(new ArrayBuffer(rawLength));
-        for (let i = 0; i < rawLength; i++) {
-            array[i] = raw.charCodeAt(i);
-        }
-        return array;
-    }
     readBlobAsDataURL(blob: Blob, callback: Function) {
-        let a = new FileReader();
-
-        a.onload = (e) => { callback(e.target); };
-        a.readAsDataURL(blob);
+        let fileReader = new FileReader();
+        fileReader.onload = (e) => { callback(e.target); };
+        fileReader.readAsDataURL(blob);
     }
-    async getPdfStream() {
-        await this.getPdf();
-        return this.default_url;
+
+    fileUrl: string;
+    getPdfBlobUrl(baseUrl: string, fileType: string) {
+        let requestHeaders = new HttpHeaders();
+        requestHeaders.append('Content-Type', 'application/json');
+        requestHeaders.append('Accept', 'q=0.8;application/json;q=0.9');
+
+        if (typeof (Storage) !== "undefined") {
+            let token = localStorage.getItem("jwt_token");
+            requestHeaders.append('Authorization', token);
+        }
+
+        this.httpClient.get('/api/Users/GetStreamPdf',
+            { headers: requestHeaders, responseType: "arraybuffer" })
+            .subscribe(data => {
+                let blobFile = new Blob([new Uint8Array(data)], { type: "application/pdf" }); //application/octet-stream
+                this.fileUrl = URL.createObjectURL(blobFile);
+                // fileURL = encodeURIComponent(fileURL).replace("blob:http", "blob:https");
+                // fileURL = fileURL.replace("%3A9090", "");
+                this.pdfSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.fileUrl);
+                this.pdfViewerSrc = this.sanitizer.bypassSecurityTrustResourceUrl("/assets/pdfjs/web/viewer.html?file=" + this.fileUrl);
+            });
+    }
+    getDefaultUrl() {
+        let requestHeaders = new HttpHeaders();
+        requestHeaders.append('Content-Type', 'application/json');
+        requestHeaders.append('Accept', 'q=0.8;application/json;q=0.9');
+        if (typeof (Storage) !== "undefined") {
+            let token = localStorage.getItem("jwt_token");
+            requestHeaders.append('Authorization', token);
+        }
+        this.httpClient.get('/label.pdf',
+            { headers: requestHeaders, responseType: "arraybuffer" })
+            .subscribe(data => {
+                let file = new Blob([new Uint8Array(data)], { type: "application/pdf" });
+                this.fileUrl = window.URL.createObjectURL(file);
+                this.pdfSrc = this.sanitizer.bypassSecurityTrustResourceUrl(this.fileUrl);
+                this.pdfViewerSrc = this.sanitizer.bypassSecurityTrustResourceUrl("/assets/pdfjs/web/viewer.html?file=" + this.fileUrl);
+            });
     }
 }
